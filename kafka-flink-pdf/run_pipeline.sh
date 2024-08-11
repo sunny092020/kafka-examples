90016b10a5f33d0f357a2c9b84f1dbb02f19af05
vu <itworkjp@gmail.com>
2024-08-11 17:25:18 +0700

    fix

--- kafka-flink-pdf/run_pipeline.sh
+++ kafka-flink-pdf/run_pipeline.sh
@@ -3,16 +3,12 @@
 # Ensure the script stops on the first error
 set -e
 
-# Run the Kafka producer
-echo "Running Kafka producer..."
-docker-compose run --rm producer
-
 # Submit Flink jobs
 echo "Submitting Flink jobs..."
-docker-compose run --rm flink_jobs flink run -py split_job.py
-docker-compose run --rm flink_jobs flink run -py map_job.py
-docker-compose run --rm flink_jobs flink run -py pdf_printer_job.py
-docker-compose run --rm flink_jobs flink run -py combine_pdf_job.py
+docker-compose exec flink_jobs flink run -py split_job.py
+docker-compose exec flink_jobs flink run -py map_job.py
+docker-compose exec flink_jobs flink run -py pdf_printer_job.py
+docker-compose exec flink_jobs flink run -py combine_pdf_job.py
 
 echo "Pipeline completed successfully."
 
