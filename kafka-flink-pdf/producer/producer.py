177c06eb9a1e5f97f47a0a5061d3f6700c458c69
vu <itworkjp@gmail.com>
2024-08-11 16:11:17 +0700

    fix

--- kafka-flink-pdf/producer/producer.py
+++ kafka-flink-pdf/producer/producer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaProducer
 import json
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers='kafka:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 data = {
     "html_template": "<html><body>{content}</body></html>",
